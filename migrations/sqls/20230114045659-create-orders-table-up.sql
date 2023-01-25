857d987de1fb80d9f8d63c9476c09550907d3d67
Jeffrey Ricardo <jeffreyricardo@gmail.com>
2023-01-25 01:50:01 -0500

    Fixed error

--- migrations/sqls/20230114045659-create-orders-table-up.sql
+++ migrations/sqls/20230114045659-create-orders-table-up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE orders (
     id SERIAL PRIMARY KEY,
     user_id BIGINT NOT NULL REFERENCES users(id),
-    status NOT NULL VARCHAR(64),
+    status VARCHAR(64) NOT NULL,
     createdAt TIMESTAMP,
     updatedAt TIMESTAMP
 );
