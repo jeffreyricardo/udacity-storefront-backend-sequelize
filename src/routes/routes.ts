823c196f59467378257e2d882079ac52c1c9fb81
Jeffrey Ricardo <jeffreyricardo@gmail.com>
2023-01-26 12:45:35 -0500

    Updated/Added erroneous routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,8 +1,8 @@
 //@ts-nocheck
 import express from "express";
 import {getUsers, getUser, createUser, updateUser, loginUser, deleteUser, verifyAuthToken} from '../handlers/userHandler';
-import {getProducts, getProduct, createProduct, updateProduct, getProductsByCategory} from '../handlers/productHandler';
-import {getOrders, getOrder, createOrder, addProduct, getOrdersByUserId} from '../handlers/orderHandler';
+import {getProducts, getProduct, createProduct, updateProduct, getProductsByCategory, deleteProduct} from '../handlers/productHandler';
+import {getOrders, getOrder, createOrder, addProduct, getOrdersByUserId, updateOrder} from '../handlers/orderHandler';
 
 const router =  express.Router();
 
@@ -16,16 +16,18 @@ router.get('/products', getProducts);
 router.post('/products/create', verifyAuthToken, createProduct);
 router.get('/products/:id', getProduct);
 router.put('/products/:id', updateProduct);
+router.delete('/products/:id', deleteProduct);
 router.get('/products/category/:id', getProductsByCategory);
 
 
 router.get('/orders', getOrders);
 router.get('/orders/:id', getOrder);
+router.put('/orders/:id', updateOrder);
 router.post('/orders/create', createOrder);
 router.post('/orders/:id/products', addProduct);
 
 // Current order by user id
-router.get('/orders/users/:id', getOrdersByUserId);
+router.get('/orders/users/:id', verifyAuthToken, getOrdersByUserId);
 
 router.post('/login', loginUser);
 
