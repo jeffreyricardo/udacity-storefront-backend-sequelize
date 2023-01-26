5198d7ebf4940c7c30106e627c64eb4a6d3d878f
Jeffrey Ricardo <jeffreyricardo@gmail.com>
2023-01-26 12:45:56 -0500

    Updated for test env

--- src/database.ts
+++ src/database.ts
@@ -6,15 +6,7 @@ dotenv.config();
 const {
     POSTGRES_HOST,
     POSTGRES_DB,
+    POSTGRES_DB_TEST,
     POSTGRES_USER,
     POSTGRES_PASSWORD,
 } = process.env;
-
-const client = new Pool({
-    host: POSTGRES_HOST,
-    database: POSTGRES_DB,
-    user: POSTGRES_USER,
-    password: POSTGRES_PASSWORD,
-});
-
-export default client;
