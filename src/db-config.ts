e29621db5f9bf0e7b0dab042b04e0faa288a068a
Jeffrey Ricardo <jeffreyricardo@gmail.com>
2023-01-26 12:43:11 -0500

    Updated for test env

--- src/db-config.ts
+++ src/db-config.ts
@@ -1,9 +1,37 @@
+//@ts-nocheck
 import { Sequelize } from "sequelize";
+import dotenv from 'dotenv';
 
-const db = new Sequelize('storefront', 'storefront_user', 'password123', {
-    host: '127.0.0.1',
-    dialect: 'postgres',
-    logging: false
-});
+dotenv.config();
+
+const {
+    NODE_ENV,
+    POSTGRES_HOST,
+    POSTGRES_DB,
+    POSTGRES_DB_TEST,
+    POSTGRES_USER,
+    POSTGRES_PASSWORD,
+} = process.env;
+
+
+let db:Sequelize;
+
+if (NODE_ENV === 'dev') {
+    console.log(`Using DEV: ${process.env.POSTGRES_DB}`);
+    db = new Sequelize(process.env.POSTGRES_DB, process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
+        host: process.env.POSTGRES_HOST,
+        dialect: 'postgres',
+        logging: false
+    });
+}
+
+if (NODE_ENV === 'test') {
+    console.log(`Using TEST: ${process.env.POSTGRES_DB_TEST}`);
+    db = new Sequelize(process.env.POSTGRES_DB_TEST, process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
+        host: process.env.POSTGRES_HOST,
+        dialect: 'postgres',
+        logging: false
+    });
+}
 
 export default db;
