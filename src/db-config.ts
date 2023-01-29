5f477b5d50bbbca0241dfcb0bd8f2095ce626bf6
Jeffrey Ricardo <jeffreyricardo@gmail.com>
2023-01-29 06:04:15 +0000

    Update db-config.ts

--- src/db-config.ts
+++ src/db-config.ts
@@ -16,14 +16,12 @@ const {
 
 let db:Sequelize;
 
-if (NODE_ENV === 'dev') {
-    console.log(`Using DEV: ${process.env.POSTGRES_DB}`);
-    db = new Sequelize(process.env.POSTGRES_DB, process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
-        host: process.env.POSTGRES_HOST,
-        dialect: 'postgres',
-        logging: false
-    });
-}
+console.log(`Using DEV: ${process.env.POSTGRES_DB}`);
+db = new Sequelize(process.env.POSTGRES_DB, process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
+    host: process.env.POSTGRES_HOST,
+    dialect: 'postgres',
+    logging: false
+});
 
 if (NODE_ENV === 'test') {
     console.log(`Using TEST: ${process.env.POSTGRES_DB_TEST}`);
